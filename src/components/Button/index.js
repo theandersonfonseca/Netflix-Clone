925547123f12b8cc7bc4eacc7b06c80d7a13cb56
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-04-25 23:30:22 -0300

    refactor: change for function expression

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container } from './styles';
 
-function Button({ children }) {
+const Button = ({ children }) => {
   return <Container>{children}</Container>;
-}
+};
 
 export default Button;
