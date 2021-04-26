b870a2aaa063527230935cd670738440a91dc32b
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-04-25 23:07:28 -0300

    style:  add theme

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  red: '#E50914',
+  lightRed: '#F40612',
+  black: '#000',
+  white: '#fff',
+
+  center: 'display: flex; justify-content: center; align-items: center',
+};
+
+export default theme;
