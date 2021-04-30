f3d77617a9d55a46b843d9b7af323bd0781bcaee
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-04-30 00:17:32 -0300

    feat: add Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Footer = () => {
+ 
+
+  return (
+  <Container>
+    <p>Dúvidas? Ligue 0800-761-4631</p>
+  </Container>
+  );
+};
+
+export default Footer;
